abef538da7a5912b844a0412e59b5e525a8052ba
mohits-git <mohitkhatri2805@gmail.com>
2024-04-06 08:04:45 +0530

    feat: solved the typescript issues and added deploy script (test)

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox";
+
+import { HardhatUserConfig } from "hardhat/config";
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+
+export default config;
